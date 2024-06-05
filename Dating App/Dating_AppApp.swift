487c5021b433b273955f2a9c9eba2d18aba6d0e3
Ishaan Goyal <goyalishaan2005@gmail.com>
2024-06-05 23:53:03 +0530

    Initial Commit

---
+++ Dating App/Dating_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dating_AppApp.swift
+//  Dating App
+//
+//  Created by Ishaan Goyal on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Dating_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
